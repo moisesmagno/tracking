3442ee36c10067abb58f46e4747974d05e0f26f2
Moisés Salvador Escurra Aguilar <moisesmagno@gmail.com>
2017-04-16 13:38:23 -0300

    Criado o controller RegisterController

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //Call the registration screem
+    public function index(){
+        return view('register');
+    }
+}
